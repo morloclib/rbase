ea8e101accd265c1183e0237aed398174feede84
Zebulun Arendsee <arendsee@iastate.edu>
2019-07-06 11:58:23 -0500

    Add HOFs
    I've included a scan function, which is missing in the C++ and python
    implementations.

---
+++ core.R
@@ -0,0 +1,28 @@
+map <- function(f, xs){
+  sapply(xs, f)  
+}
+
+zipWith <- function(f, xs, ys){
+  N <- min(length(xs), length(ys))
+  zs <- as.list(rep(NA, N))
+  for(i in seq_along(xs)){
+    zs[[i]] <- f(xs[[i]], ys[[i]])
+  }
+  return(zs)
+}
+
+enumerateWith <- function(f, xs){
+  ys <- list()
+  for(i in seq_along(xs)){
+    ys[[i]] <- f(xs[[i]], i)
+  }
+  return(ys)
+}
+
+fold <- function(f, init, xs){
+  Reduce(f=f, x=xs, init=init, accumulate=FALSE)
+}
+
+scan <- function(f, init, xs){
+  Reduce(f=f, x=xs, init=init, accumulate=TRUE)
+}
