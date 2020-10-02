e3fdaa720380221826c6f19db240c72c4ae34fd6
Zebulun Arendsee <arendsee@iastate.edu>
2020-10-01 19:29:59 -0500

    Use type aliases for R types
    The type signatures for R are becoming very repetitive. I toying with
    the idea of deleting them altogether except in special cases. They can
    be inferred. For example:
    
    ```
    type R Num = "numeric";
    
    add :: Num -> Num -> Num;
    add R :: Num -> Num -> Num;
    ```
    
    Why bother with the `add` function in R? We can just infer it. This will
    work whenever there is one-to-one maps from the general to the concrete
    types.

--- R/core.R
+++ R/core.R
@@ -1,12 +1,12 @@
-id <- function(x){
+mlc_id <- function(x){
   x
 }
 
-map <- function(f, xs){
+mlc_map <- function(f, xs){
   sapply(xs, f)  
 }
 
-zipWith <- function(f, xs, ys){
+mlc_zipWith <- function(f, xs, ys){
   N <- min(length(xs), length(ys))
   zs <- as.list(rep(NA, N))
   for(i in seq_along(xs)){
@@ -15,7 +15,7 @@ zipWith <- function(f, xs, ys){
   return(zs)
 }
 
-enumerateWith <- function(f, xs){
+mlc_enumerateWith <- function(f, xs){
   ys <- list()
   for(i in seq_along(xs)){
     ys[[i]] <- f(xs[[i]], i)
@@ -23,18 +23,18 @@ enumerateWith <- function(f, xs){
   return(ys)
 }
 
-fold <- function(f, init, xs){
+mlc_fold <- function(f, init, xs){
   Reduce(f=f, x=xs, init=init, accumulate=FALSE)
 }
 
-scan <- function(f, init, xs){
+mlc_scan <- function(f, init, xs){
   Reduce(f=f, x=xs, init=init, accumulate=TRUE)
 }
 
-add <- function(x,y) x + y
-sub <- function(x,y) x - y
-mul <- function(x,y) x * y
-div <- function(x,y) x / y
+mlc_add <- function(x,y) x + y
+mlc_sub <- function(x,y) x - y
+mlc_mul <- function(x,y) x * y
+mlc_div <- function(x,y) x / y
 
 plotPDF <- function(x, filename){
   pdf(filename)
@@ -42,6 +42,6 @@ plotPDF <- function(x, filename){
   dev.off()
 }
 
-plotVectorPDF <- function(...){
+mlc_plotVectorPDF <- function(...){
   plotPDF(...)
 }
